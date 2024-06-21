ebdcab798fa3264b605bbbadb061fb804a4f6ca3
Nicolae Ionascu <nionascu@moldcell.md>
2024-06-21 16:26:54 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+const apicache = require("apicache");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
+
+const app = express();
+const cache = apicache.middleware;
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cache("2 minutes"));
+app.use("/api/v1/workouts", v1WorkoutRouter);
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
+  V1SwaggerDocs(app, PORT);
+});
